e4c9258e61d72a485d3ec9a05d4d8b7ce1debec2
kkuzie <kkuzii@yahoo.it>
2020-04-08 22:54:45 -0700

    Final Changes

--- Assignment 6 JS/js/employee-management.js
+++ Assignment 6 JS/js/employee-management.js
@@ -1,21 +1,12 @@
 /*eslint-env browser*/
 
-const addName = document.querySelector('.name');
-const addTitle = document.querySelector('.title');
-const addExt = document.querySelector('.ext');
-
+const addInput = document.forms.addInput;
+//this grabs the forms tag just like getElementsByClassName, etc
 const table = document.querySelector('tbody.list');
 
 const search = document.querySelector('.search input');
 
-let count  = document.querySelector(".table");
-let totalRowCount = 0;
-let rowCount = 0;
-
-
-// const formHeader = document.querySelector('.add-input');
-// const count = document.querySelector('.count');
-
+//template to add new row when employee info added
 const generateTemplate = ((name, title, ext) => {
     const html = `
     <tr>
@@ -31,38 +22,122 @@ const generateTemplate = ((name, title, ext) => {
     table.innerHTML += html;
 });
 
-addName.addEventListener('submit', e => {
+// //adding employee info
+// const addEmpl = document.forms['addInput'];
+//addEmpl is same as addInput var, line 3
+
+let btn = document.getElementById('addBtn');
+
+btn.addEventListener('focus', e => {
     e.preventDefault();
-    const name = addName.name.value.trim();
-    console.log(name);
+    const addName = addInput.querySelector('input[name="name"]').value.trim();
+    console.log(addName);
+    if (addName == '') {//when here keeps adding to list but turns red upon enter or add -and cursor stays in place with enter
+        addInput.name.style.border = '3px solid crimson';
+        //stays red when no input but still adds to list empty string
 
-        addTitle.addEventListener('submit', e => {
-            e.preventDefault();
-            const title = addTitle.title.value.trim();
-            console.log(title);
+    }
 
-                addExt.addEventListener('submit', e => {
-                    e.preventDefault();
-                    const ext = addExt.extension.value.trim();
-                    console.log(ext);
+    const addTitle = addInput.querySelector('input[name="title"]').value.trim();
+    console.log(addTitle);
+    if (addTitle == '') {
+        addInput.title.style.border = '3px solid crimson';//turns red at same time as enter or add from employee name
+        //stays red when no input but still adds to list empty string
+    }
 
-// this needs to be fixed
-                    if(name.length || title.length || ext.length){
-                    generateTemplate(name, title, ext);
-                    addName.reset();
-                    addTitle.reset();
-                    addExt.reset();
-                    }
-                });
-        });
+    const addExt = addInput.querySelector('input[name="extension"]').value.trim();
+    console.log(addExt);
+    if (addExt == '') {
+        addInput.extension.style.border = '3px solid crimson';
+    }
+
+    //all three text boxes dont turn red if push tab to get to next box, but then all turn red upon submit (enter or add) and still add empty line
+
+    if (addName !== '' && addTitle !== '' && addExt !== '') {
+        generateTemplate(addName, addTitle, addExt);
+        addInput.reset();
+    }
+    counterFunc();
 });
+
+
+
+
+
+// addInput.addEventListener('submit', e => {
+//     e.preventDefault();
+//     const addName = addInput.querySelector('input[name="name"]').value.trim();
+//     console.log(addName);
+//         if(addName == ''){//when here keeps adding to list but turns red upon enter or add -and cursor stays in place with enter
+//             addInput.name.style.border = '3px solid crimson';
+//                         //stays red when no input but still adds to list empty string
+//         }
+
+//     const addTitle = addInput.querySelector('input[name="title"]').value.trim();
+//     console.log(addTitle);
+//         if(addTitle == ''){
+//             addInput.title.style.border = '3px solid crimson';//turns red at same time as enter or add from employee name
+//             //stays red when no input but still adds to list empty string
+//         }
+
+//     const addExt = addInput.querySelector('input[name="extension"]').value.trim();
+//     console.log(addExt);
+//         if(addExt == ''){
+//             addInput.extension.style.border = '3px solid crimson';
+//         }
+
+//         //all three text boxes dont turn red if push tab to get to next box, but then all turn red upon submit (enter or add) and still add empty line
+
+//     generateTemplate(addName, addTitle, addExt);
+//     // addInput.onsubmit = function() {
+//     //     if(addName == ''){
+//     //         addInput.name.style.border = '3px solid crimson'; 
+//     //         return false;
+//     //     // } else if (addName != ''){
+//     //     //     addInput.name.style.border = '3px solid limegreen';
+//     //     //     return false;
+//     //     } else {
+//     //         // addInput.name.style.border = 'none';
+//     //         return true;
+//     //     }
+//     // };
+    
+//     addInput.reset();
+//     counterFunc();
+// });
+
+// adding employee focus
+addInput.name.onfocus = function(){
+    addInput.name.style.border = '3px solid limegreen'; 
+};
+addInput.name.onblur = function(){
+    addInput.name.style.border = 'none';
+}
+
+addInput.title.onfocus = function(){
+    addInput.title.style.border = '3px solid limegreen'; 
+};
+addInput.title.onblur = function(){
+    addInput.title.style.border = 'none';
+}
+
+addInput.extension.onfocus = function(){
+    addInput.extension.style.border = '3px solid limegreen'; 
+};
+addInput.extension.onblur = function(){
+    addInput.extension.style.border = 'none';
+}
+
+
+//removing row when employee deleted
 table.addEventListener('click', e => {
     if(e.target.classList.contains('delete')){
         e.target.parentElement.parentElement.remove();
+        counterFunc();
     }
 });
 
-
+//add & remove filtered class
 const filterEmpl = (term) => {
     Array.from(table.children)
         .filter((empl) => {
@@ -75,54 +150,46 @@ Array.from(table.children)
     return empl.textContent.toLowerCase().includes(term);
 })
 .forEach((empl) => empl.classList.remove('filtered'));
+  counterFunc();
 };
-
-//keyup event
+     
+//keyup event for search
 search.addEventListener('keyup', () => {
     const term = search.value.trim().toLowerCase();
     filterEmpl(term);
+  
 });
 
+//row counter:
+let count = document.querySelector('tbody');//grab table
 
-// formHeader.addEventListener('submit', e => {
-//     e.preventDefault();
-// }) 
-//     //need to add way to add # of employees in list
-//     let emplCount = 0;
-//     const addedEmpl = [formHeader.name.value, formHeader.title.value, formHeader.extension.value];
+let rows = count.getElementsByTagName("tr");
+console.log(rows);//grab rows collection
 
-//     addedEmpl.forEach((input, index) => {
+let empl = Array.from(count.children).length;
+console.log(empl);//change table to array and give length
 
-//     });
+let numEmpl = document.querySelector('.table-rows');//so can grab span
 
-//row counter
-function CountRows() {
-    // let totalRowCount = 0;
-    // let rowCount = 0;
-    // let count  = document.querySelector(".table");
-    console.log(count);
+    numEmpl.querySelector('span').textContent = `${rows.length}`;//shows number of row in browser
 
-    let rows = count.getElementsByTagName("tr")
-        for (let i = 0; i < rows.length; i++) {
-            totalRowCount++;
-                if (rows[i].getElementsByTagName("td").length > 0 && rows[i].getElementsByTagName('th').length > 0) {
-                rowCount++;
-            }
-        console.log(rows);
+//try try again - row counter:
+//taken from: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_nodelist_length_p
+function counterFunc() {
+//     let count = document.querySelector('tbody');//grab table
+// console.log(count);
+
+// let rows = count.getElementsByTagName("tr");
+// console.log(rows);//grab rows collection
+// let numEmpl = document.querySelector('.table-rows');//so can grab span
+
+let filtered = document.getElementsByClassName('filtered');
+let result = Number(rows.length - filtered.length);
+
+    if(filtered.length >= 0){
+            numEmpl.querySelector('span').innerHTML = `${result}`;
     }
-
-    // let message = rowCount;
-    // console.log(message);
-
-//show # employees
-    // count.querySelector('span').textContent = `${message}`;
+    else {
+        numEmpl.querySelector('span').innerHTML = `${rows.length}`;
+  }
 }
-CountRows();
-let message = rowCount;
-console.log(message);
-//need to figure out how to increase row count when employee added
-//and decrease row count when employee deleted
-
-
-count.querySelector('span').textContent = `${message}`;
-
